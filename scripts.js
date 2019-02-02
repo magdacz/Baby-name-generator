2be72055abedd55d530f99f4571dd8b2e33ca82e
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-02-02 19:55:02 +0100

    Add css, colors functions

--- scripts.js
+++ scripts.js
@@ -8,23 +8,44 @@ let counter = 0;
 const girls = ['Zuzanna', 'Julia', 'Zofia', 'Maja', 'Hanna', 'Lena', 'Alicja', 'Maria', 'Amelia', 'Oliwia', 'Justyna', 'Oliwia', 'Elżbieta', 'Emilia', 'Anna', 'Helena', 'Nadia', 'Michalina', 'Aleksandra', 'Wiktoria', 'Natalia'];
 const boys = ['Antoni', 'Jakub', 'Jan', 'Szymon', 'Aleksander', 'Franciszek', 'Filip', 'Wojciech', 'Mikołaj', 'Kacper', 'Remigiusz', 'Norbert', 'Dawid', 'Miłosz', 'Bartosz', 'Karol', 'Adam', 'Stanisław', 'Marcel', 'Michał'];
 
+const girlsColors = () => {
+    result.classList.remove('orange');
+    result.classList.remove('blue');
+    result.classList.add('pink');
+    
+    if(counter % 2 === 0) {
+    result.classList.remove('pink');
+    result.classList.add('yellow');
+  } else {
+    result.classList.remove('yellow');
+    result.classList.add('pink');
+  }
+}
+
+const boysColors = () => {
+    result.classList.remove('pink');
+    result.classList.remove('yellow');
+    result.classList.add('blue');
+    
+    if(counter%2 === 0) {
+    result.classList.remove('blue');
+    result.classList.add('orange');
+  } else {
+    result.classList.remove('orange');
+    result.classList.add('blue');
+  }
+}
+
 const randomGirlName = () => {
   ++counter;
   
   const index1 = Math.floor(Math.random() * girls.length);
   const girl = girls[index1];
     
-  info.textContent = 'Wylosowane imie dla Twojego dziecka to';
-  result.classList.add('red');
-    
-  if(counter % 2 === 0) {
-    result.classList.remove('red');
-    result.classList.add('green');
-  } else {
-    result.classList.remove('green');
-    result.classList.add('red');
-  }
-    
+  info.textContent = 'Wylosowane imię dla Twojego dziecka to:';
+  
+  girlsColors(); 
+  
   result.textContent = girl;
 }
 
@@ -34,16 +55,9 @@ const randomBoyName = () => {
   const index2 = Math.floor(Math.random() * boys.length);
   const boy = boys[index2];
   
-  info.textContent = 'Wylosowane imie dla Twojego dziecka to';
-  result.classList.add('red');
-    
-  if(counter%2 === 0) {
-    result.classList.remove('red');
-    result.classList.add('green');
-  } else {
-    result.classList.remove('green');
-    result.classList.add('red');
-  }
+  info.textContent = 'Wylosowane imię dla Twojego dziecka to:';
+  
+   boysColors(); 
   
   result.textContent = boy;
 }
